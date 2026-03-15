87eefa986c762144cbb99ac8e2fa10079d0f1428
Hazal GUN <nhazalsahin@gmail.com>
2026-03-15 11:32:40 +0300

    Atama dosyası güncellendi

--- assignments.py
+++ assignments.py
@@ -1,3 +1,11 @@
 numbers = 1,2,3
 x,y,z = numbers
 print(x,y,z)  # x=1 y=2 z=3  değer  sayısı ile değişken sayısı eşit olmalı
+
+numbers= 1,2,3,4,5,6,7,8,9
+x,*y,z = numbers
+
+print(x,y,z) # x ilk z son değeri alır ve y ortada kalan tüm değerleri alır
+# x=1
+# y=[2,3,4,5,6,7,8]
+# z=9
